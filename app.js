7b0d075a06277d4b21a48bb064caede64d2a6b83
rayback00001 <rabrosrajgopu@gmail.com>
2022-09-12 21:22:33 +0200

    added queryselectors

---
+++ app.js
@@ -0,0 +1,5 @@
+let seconds = document.querySelector('.seconds');
+let milli = document.querySelector('.milli');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
