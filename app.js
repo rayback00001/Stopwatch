34e3edc6184275548aa5b20abead1ceb817744dd
Rakul Rajgopu <rabrosrajgopu@gmail.com>
2022-09-13 10:10:19 +0200

    update

--- app.js
+++ app.js
@@ -6,6 +6,37 @@ let btnReset = document.querySelector('.btn-reset');
 let seconds = 0;
 let milli = 0;
 
+
+
+btnStart.addEventListener('click',()=>{
+    milli++;
+    if(milli <= 9)
+    {
+        Milli.innerHTML = '0' + milli;
+    }
+
+    else if(milli > 9)
+    {
+        Milli.innerHTML = milli;
+    }
+
+    if(milli > 99)
+    {
+        milli = 0;
+        Milli.innerHTML = '0' + 0;
+        seconds++;
+        Seconds.innerHTML = '0' + seconds;
+        
+    }
+
+    if(seconds > 9)
+    {
+        Seconds.innerHTML = seconds;
+    }
+});
+
+
+/*
 function startTimer(){
     milli++;
     if(milli <= 9)
@@ -29,6 +60,5 @@ function startTimer(){
         Seconds.innerHTML = seconds;
     }
 
-
-
 }
+*/
